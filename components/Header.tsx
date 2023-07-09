73d7486cf2556e7938be595f369b288c0505646d
Nigel <nigeltakadza@gmail.com>
2023-07-09 22:00:41 +0200

    initai commit

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Header() {
+
+  return (
+
+     <header className='sticky top-0 z-50 grid grid-cols-3 bg-gray-200 shadow-md p-6 md:px-20'>
+     <h1 className='font-bold text-xl'>Nampo Survey</h1>
+   
+   </header>
+  )
+}
+
+export default Header
